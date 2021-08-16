45862439c1dc8a0f0ff5b8341f65cdedeed4f829
Snehil Jain <snehil.udrhj@gmail.com>
2021-08-17 00:37:03 +0530

    done deployment

---
+++ client/src/components/footers/Footer.js
@@ -0,0 +1,18 @@
+import React, { useContext, useState } from 'react'
+import './footer.css';
+
+
+
+function Footer() {
+    return(
+    <div className="footer">
+        <span className="credits">Made with ❤️ by <a href="http://snehiljain34.ml/" className="credits-a">SNEHIL JAIN </a> </span> 
+        <span className="credits">
+            <a href="https://www.instagram.com/_snehil.codes/" >@_snehil.codes</a> 
+        </span>
+    </div>
+
+);
+}
+
+export default Footer;
